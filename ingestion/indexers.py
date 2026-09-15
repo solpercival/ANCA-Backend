ac73b3c56d885efef21c578612d7568cf0867e55
Yee Shun Chew <yee.chew@student.unimelb.edu.au>
2026-09-15 18:20:23 +1000

    add: table validation logic before insert

--- ingestion/indexers.py
+++ ingestion/indexers.py
@@ -2,6 +2,7 @@
 import json
 from FlagEmbedding import BGEM3FlagModel
 from pathlib import Path
+from collections import defaultdict
 
 # ingestion/indexers.py
 from rag_engine.providers import get_lexical_backend
@@ -47,36 +48,86 @@ def _unified_embed(chunks: list[RawChunk], model: BGEM3FlagModel) -> dict[str,li
     output = model.encode([chunk.text for chunk in chunks], return_dense=True, return_sparse=True)
 
     return {"dense": output["dense_vecs"].tolist(), "sparse": output["lexical_weights"]}
+
+def insert_document(cursor, version: str, hash: str, file_path: str) -> int:
+    res = cursor.execute(
+        """
+        INSERT INTO document (current_version, hash, file_path)
+        VALUES (%s, %s, %s)
+        RETURNING doc_id
+        """,
+        (version, hash, file_path)
+    ).fetchone()
+
+    return res[0]
+
+def validate_tables(cursor) -> None:
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS document_chunks (
+            chunk_id BIGINT PRIMARY KEY,
+            content TEXT NOT NULL,
+            metadata JSONB NOT NULL DEFAULT '{}'::jsonb,
+            dc_type CHUNK_TYPE NOT NULL DEFAULT 'text',
+            document_chunkscol VARCHAR(45) NOT NULL,
+            lexical_embedding sparsevec(250002) NOT NULL,
+            semantic_embedding vector(1024) NOT NULL
+        )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS document (
+            doc_id INTEGER GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            current_version VARCHAR(16) NOT NULL,
+            hash CHAR(64) NOT NULL,
+            file_path VARCHAR(120) NOT NULL
+        )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS heading (
+            heading_id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+            order VARCHAR(45) NOT NULL,
+            hierarchy VARCHAR(45) NOT NULL,
+            document_id INTEGER REFERENCES document(doc_id) NOT NULL
+        )
+        """
+    )
+    cursor.execute(
+        """
+        CREATE TABLE IF NOT EXISTS chunk_headings (
+            heading_id BIGINT REFERENCES heading(heading_id) NOT NULL,
+            doc_chunks_id BIGINT REFERENCES document_chunks(chunk_id) NOT NULL,
+            PRIMARY KEY (heading_id, doc_chunks_id)
+        )
+        """
+    )
     
+
 def _write_embeddings(chunks: list[RawChunk], dense_embeddings: list[list[float]], sparse_embeddings: list[dict[str,float]]) -> None:
+    doc_chunks: dict[str, list[RawChunk]] = defaultdict(list)
+
+    for chunk in chunks:
+        doc_chunks[chunk.source].append(chunk)
+
     settings = get_settings()
     with psycopg.connect(settings.postgres_dsn) as connection:
         register_vector(connection)
         with connection.cursor() as cursor:
-            # check if table created
-            cursor.execute(
-                """
-                CREATE TABLE IF NOT EXISTS document_chunks (
-                    chunk_id BIGINT PRIMARY KEY,
-                    content TEXT NOT NULL,
-                    metadata JSONB NOT NULL DEFAULT '{}'::jsonb,
-                    dc_type CHUNK_TYPE NOT NULL DEFAULT 'text',
-                    document_chunkscol VARCHAR(45) NOT NULL,
-                    lexical_embedding sparsevec(250002) NOT NULL,
-                    semantic_embedding vector(1024) NOT NULL
-                )
-                """
-            )
+            # check if tables created
+            validate_tables(cursor=cursor)
 
-            insert_data = [(chunk.text, '{}', chunk.kind, "", sparse, dense) for 
+            for doc in doc_chunks:
+                doc_id = insert_document(cursor=cursor, version=1, hash="", file_path=doc)
+
+                for chunk in doc_chunks[doc]:
+                    heading_ids = 
+
+            chunk_insert_data = [(chunk.text, '{}', chunk.kind, "", sparse, dense) for 
                            chunk, sparse, dense in zip(chunks, sparse_embeddings, dense_embeddings)]
-
-            cursor.execute(
-                """
-                INSERT INTO document_chunks (content, metadata, dc_type, document_chunkscol, lexical_embedding, semantic_embedding)
-                VALUES (%s, %s, %s, %s, %s, %s)
-                """
-            )
+            
 
 def embed_and_index(chunks: list[RawChunk]) -> None:  # pragma: no cover - integration
     if not chunks:
